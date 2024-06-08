ca67083183ea71c0ef099df335d03afc973ffdf4
lyndajung <eunhae_87@live.com.au>
2024-06-08 21:07:41 +1000

    variables

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,8 +8,22 @@ int y = 321; //declaration + initialisation
 int z = x + y;
 int j = z - y;
 
+int age = 21; //whole number
+double height = 300.5;
+char symbol = '@';
+String name = "Lynda";
+
 Console.WriteLine(x);
 Console.WriteLine(y);
 Console.WriteLine(z);
 Console.WriteLine(j);
 Console.WriteLine(z+j);
+Console.WriteLine("your age is " + age);
+
+Console.WriteLine("Your height is " + height + "cm");
+Console.WriteLine("Your symbol is: " + symbol);
+
+String userName = symbol + name;
+
+Console.WriteLine("Your username is: " + userName);
+Console.Beep();
