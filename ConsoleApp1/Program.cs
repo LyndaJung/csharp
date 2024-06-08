b0fee5d5de37c7e273e54a5bffd69a5985aaa04e
lyndajung <eunhae_87@live.com.au>
2024-06-08 20:46:25 +1000

    hello

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("I like Pizza!");
+Console.WriteLine("It's really good!!");
+Console.ReadKey();
+Console.Beep();
+
+//This is a comment
+
+
+
