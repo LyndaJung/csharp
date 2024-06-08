132d7887e0ede6c90c8f463abc78bf8f36cf8840
lyndajung <eunhae_87@live.com.au>
2024-06-08 21:29:28 +1000

    typecasting

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,6 @@ x = 123; //initialisation
 int y = 321; //declaration + initialisation
 
 int z = x + y;
-int j = z - y;
 
 int age = 21; //whole number
 double height = 300.5;
@@ -16,8 +15,7 @@ String name = "Lynda";
 Console.WriteLine(x);
 Console.WriteLine(y);
 Console.WriteLine(z);
-Console.WriteLine(j);
-Console.WriteLine(z+j);
+
 Console.WriteLine("your age is " + age);
 
 Console.WriteLine("Your height is " + height + "cm");
@@ -26,4 +24,30 @@ Console.WriteLine("Your symbol is: " + symbol);
 String userName = symbol + name;
 
 Console.WriteLine("Your username is: " + userName);
+
+// type casting = Converting a value to a different data type
+//                Useful when we accept user input (string)
+//                Different data types can do different things
+
+double a = 3.14;
+int b = Convert.ToInt32(a);
+
+int c = 123;
+double d = Convert.ToDouble(c);
+
+int e = 321;
+String f = Convert.ToString(e);
+
+String g = "$";
+char h = Convert.ToChar(g);
+
+String i = "true";
+bool j = Convert.ToBoolean(i);
+
+Console.WriteLine(b.GetType());
+Console.WriteLine(d.GetType());
+Console.WriteLine(f.GetType());
+Console.WriteLine(h.GetType());
+Console.WriteLine(j.GetType());
+
 Console.Beep();
